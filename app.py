3d2ae9a18fc045175214abcd0bb39e602750675f
dlarkins24 <137289283+dlarkins24@users.noreply.github.com>
2023-10-10 13:56:14 +0000

    cors

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ import uuid
 app = Flask(__name__)
 
 # Enable CORS for specific origins (for production and development)
-CORS(app, resources={r"/*": {"origins": ["https://brave-coast-00c913710.3.azurestaticapps.net"]}})
+CORS(app, resources={r"/*": {"origins": ["https://brave-coast-00c913710.3.azurestaticapps.net"]}}, supports_credentials=True)
 
 @app.route('/start-session', methods=['POST'])
 def start_session():
-    # Generate a unique session ID
-    session_id = str(uuid.uuid4())
-    
-    # Optionally: Store session_id and additional data in a database
-    # ...
+    try:
+        # Generate a unique session ID
+        session_id = str(uuid.uuid4())
+        
+        # Optionally: Store session_id and additional data in a database
+        # ...
 
-    # Send the session ID back to the frontend
-    return jsonify({"sessionId": session_id})
+        # Send the session ID back to the frontend
+        return jsonify({"sessionId": session_id}), 201  # 201 Created
+
+    except Exception as e:
+        app.logger.error(f"Error creating session: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500  # 500 Internal Server Error
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
