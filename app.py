0dea5085cd5ef73fc80437b2105328f1a650b040
dlarkins24 <137289283+dlarkins24@users.noreply.github.com>
2023-10-10 10:09:37 +0000

    welcomepave 2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import uuid
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all domains
+CORS(app)  # Enable CORS for all domans
 
 @app.route('/start-session', methods=['POST'])
 def start_session():
